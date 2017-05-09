4fc0ec072df9b1d40c6bfd5d910024eeb9b8a92a
Cees de Groot <casedeg@gmail.com>
2017-05-08 22:03:48 -0400

    Add some more experimental DbC code

--- apps/simpler/lib/simpler/contract.ex
+++ apps/simpler/lib/simpler/contract.ex
@@ -14,29 +14,42 @@ defmodule Simpler.Contract do
 
   defmacro def(name_and_args, body) do
     Logger.error("Some def! #{inspect(name_and_args)}")
-    Logger.error("     def! #{inspect body}")
-    Logger.error("     pre: #{inspect Module.get_attribute(__CALLER__.module, :__pre__)}")
-    Logger.error("    post: #{inspect Module.get_attribute(__CALLER__.module, :__post__)}")
     {name, _, args} = name_and_args
+    key = {name, length(args)}
+    current_key = Module.get_attribute(__CALLER__.module, :__contracted_fn__) || key
+    Logger.error("Key: #{inspect key}, current_key: #{inspect current_key}")
+    if key != current_key do
+      Logger.error("Wiping #{inspect key} not is #{inspect current_key}")
+      Module.delete_attribute(__CALLER__.module, :__pre__)
+      Module.delete_attribute(__CALLER__.module, :__post__)
+      Module.delete_attribute(__CALLER__.module, :__contracted_fn__)
+    else
+      Module.put_attribute(__CALLER__.module, :__contracted_fn__, key)
+    end
     do_block = body[:do]
     Logger.error("      do: #{inspect do_block}")
     pre_block = Module.get_attribute(__CALLER__.module, :__pre__) || [do: true]
     post_block = Module.get_attribute(__CALLER__.module, :__post__) || [do: true]
+    Logger.error("     pre: #{inspect pre_block}")
+    Logger.error("    post: #{inspect post_block}")
     quote do
       Kernel.def unquote(name)(unquote_splicing(args)) do
         assert unquote(pre_block[:do])
-        unquote(do_block)
+        # TODO cleanest method to grab the result?
+        var!(result) = if true do
+          unquote(do_block)
+        end
+        assert unquote(post_block[:do])
+        var!(result)
       end
     end
   end
 
   defmacro precondition(stuff) do
-    Logger.error("Precondition! #{inspect stuff}")
     Module.put_attribute(__CALLER__.module, :__pre__, stuff)
   end
 
   defmacro postcondition(stuff) do
-    Logger.error("Postcondition! #{inspect stuff}")
     Module.put_attribute(__CALLER__.module, :__post__, stuff)
   end
 
--- apps/simpler/test/contract_test.exs
+++ apps/simpler/test/contract_test.exs
@@ -7,18 +7,68 @@ defmodule Simpler.ContractTest do
     def mymethod(op_one, op_two) do
       op_one - op_two
     end
+    def nextmethod(op_one, op_two) do
+      op_one + op_two
+    end
   end
   test "Contracted method accepts preconditions" do
     assert SimplePre.mymethod(4, 2) == 2
   end
-  test "Contracted method reject bad precondition" do
+  test "Next method doesn't inherit preconditions" do
+    assert SimplePre.nextmethod(2, 4) == 6
+  end
+  test "Contracted method reject :33
+  bad precondition" do
     try do
-      SimplePre.mymethod(2, 4) == -2
+      SimplePre.mymethod(2, 4)
       raise "assertion didn't trigger!"
     rescue
-      e in [ExUnit.AssertionError] -> nil
+      _e in [ExUnit.AssertionError] -> nil
       e -> raise(e)
     end
   end
 
+  defmodule SimplePost do
+    use Simpler.Contract
+    postcondition do: result > 0
+    def mymethod(op_one, op_two) do
+      op_one - op_two
+    end
+    def nextmethod(op_one, op_two) do
+      op_one + op_two
+    end
+  end
+  test "Contracted method accepts postcondition" do
+    assert SimplePost.mymethod(4, 2) == 2
+  end
+  test "Next method doesn't inherit postonditions" do
+    assert SimplePre.nextmethod(-2, -4) == -6
+  end
+  test "Contract method rejects postcondition" do
+    try do
+      SimplePost.mymethod(2, 4)
+      raise "assertion didn't trigger!"
+    rescue
+      _e in [ExUnit.AssertionError] -> nil
+      e -> raise(e)
+    end
+  end
+
+  defmodule MultipleHeads do
+    use Simpler.Contract
+    precondition do: op_two < 10
+    postcondition do: result > 0
+    def method(:one, op_two) do
+      10 + op_two
+    end
+    def method(:two, op_two) do
+      20 + op_two
+    end
+    def method(op_one, op_two) do
+      30 + op_two
+    end
+  end
+  test "Multiple function heads work" do
+    assert MultipleHeads.method(:one, 1) == 11
+  end
 end
