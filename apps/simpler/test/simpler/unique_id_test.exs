04a3654d985ece9ed2ca6a7d5456812c32bebde5
Cees de Groot <casedeg@gmail.com>
2017-05-23 20:36:15 -0400

    Add another unique id test

--- apps/simpler/test/simpler/unique_id_test.exs
+++ apps/simpler/test/simpler/unique_id_test.exs
@@ -7,5 +7,9 @@ defmodule Simpler.UniqueIdTest do
     id = UniqueId.unique_id()
     string_id = UniqueId.to_string(id)
     assert UniqueId.from_string(string_id) == id
+
+    id_string = UniqueId.unique_id_string()
+    id = UniqueId.from_string(id_string)
+    assert UniqueId.to_string(id) == id_string
   end
 end
