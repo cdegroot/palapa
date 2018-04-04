74a61a00aa1dfc6a35b371a7e51c65e9817764f7
Cees de Groot <casedeg@gmail.com>
2018-04-03 23:12:52 -0400

    Add README for vectorgraphix

---
+++ apps/vectorgraphix/lib/vectorgraphix.ex
@@ -0,0 +1,18 @@
+defmodule Vectorgraphix do
+  @moduledoc """
+  Documentation for Vectorgraphix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vectorgraphix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
