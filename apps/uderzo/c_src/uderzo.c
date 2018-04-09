6fdd4f1e551ac07adc8eb574df34f43ca1c997f1
Cees de Groot <casedeg@gmail.com>
2018-04-09 19:19:06 -0400

    Start the actual work

--- apps/uderzo/c_src/uderzo.c
+++ apps/uderzo/c_src/uderzo.c
@@ -11,6 +11,22 @@
 
 #include <erl_interface.h>
 
+// Comment-driven development. 
+// 1. Library initialization is done at program startup time. 
+// 1a.For now, there's one global (vg/gl) context.
+// 2. Everything else is manual. 
+// 3. For now, we openly admit to using nanovg, glfw, opengl es
+// 3a.So we directly map functions on the protocol, no translation
+// 3b.This should make code generation simpler, eventually
+// 3c.We also don't wrap pointers. They're just opaque handles on
+//    the BEAM side.
+// 4. The render loop is BEAM-side, this code is passive
+// 5. Any GLFW keyboard and mouse events are sent to stdout, async
+// 6. Hence, the stdin/stdout protocol needs to be async
+// 7. Mouse state can also be polled
+// 8. BEAM-side, there should be a concept of flushing so that we
+//    can batch commands. This is not visible here. 
+
 int main() {
   char buffer[MAXATOMLEN];
   int index = 0;
