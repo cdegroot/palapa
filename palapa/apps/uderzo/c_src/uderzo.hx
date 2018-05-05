375990279122450405da96cd06ea14ff22213cdb
Cees de Groot <casedeg@gmail.com>
2018-05-05 13:59:42 +0000

    Steal some setup code from VideoCore demo

--- palapa/apps/uderzo/c_src/uderzo.hx
+++ palapa/apps/uderzo/c_src/uderzo.hx
@@ -46,6 +46,41 @@
 // [ ] 8. BEAM-side, there should be a concept of flushing so that we
 //        can batch commands. This is not visible here.
 
+// For now we simply use the state structure from the VC example, whittle away
+// what we don't need. 
+#ifdef UDERZO_VC
+typedef struct
+{
+   uint32_t screen_width;
+   uint32_t screen_height;
+// OpenGL|ES objects
+   DISPMANX_DISPLAY_HANDLE_T dispman_display;
+   DISPMANX_ELEMENT_HANDLE_T dispman_element;
+   EGLDisplay display;
+   EGLSurface surface;
+   EGLContext context;
+   GLuint tex[6];
+// model rotation vector and direction
+   GLfloat rot_angle_x_inc;
+   GLfloat rot_angle_y_inc;
+   GLfloat rot_angle_z_inc;
+// current model rotation angles
+   GLfloat rot_angle_x;
+   GLfloat rot_angle_y;
+   GLfloat rot_angle_z;
+// current distance from camera
+   GLfloat distance;
+   GLfloat distance_inc;
+// pointers to texture buffers
+   char *tex_buf1;
+   char *tex_buf2;
+   char *tex_buf3;
+} CUBE_STATE_T;
+
+// Define one global. "One display should be enough for everybody" - Bill G.
+CUBE_STATE_T state;
+#endif
+
 extern void errorcb(int error, const char *desc);
 //extern void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 extern void read_loop();
@@ -58,7 +93,84 @@ DemoData data;
 int main() {
 
 #ifdef UDERZO_VC
-    // TODO setup for RPi3
+    // Stolen from the hello triangle sample
+   int32_t success = 0;
+   EGLBoolean result;
+   EGLint num_config;
+
+   static EGL_DISPMANX_WINDOW_T nativewindow;
+
+   DISPMANX_UPDATE_HANDLE_T dispman_update;
+   VC_RECT_T dst_rect;
+   VC_RECT_T src_rect;
+
+   static const EGLint attribute_list[] =
+   {
+      EGL_RED_SIZE, 8,
+      EGL_GREEN_SIZE, 8,
+      EGL_BLUE_SIZE, 8,
+      EGL_ALPHA_SIZE, 8,
+      EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+      EGL_NONE
+   };
+   
+   EGLConfig config;
+
+   // get an EGL display connection
+   state.display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+   assert(state.display != EGL_NO_DISPLAY);
+
+   // initialize the EGL display connection
+   result = eglInitialize(state.display, NULL, NULL);
+   assert(EGL_FALSE != result);
+
+   // get an appropriate EGL frame buffer configuration
+   result = eglChooseConfig(state.display, attribute_list, &config, 1, &num_config);
+   assert(EGL_FALSE != result);
+
+   state.context = eglCreateContext(state.display, config, EGL_NO_CONTEXT, NULL);
+   assert(state.context!=EGL_NO_CONTEXT);
+
+   // create an EGL window surface
+   success = graphics_get_display_size(0 /* LCD */, &state.screen_width, &state.screen_height);
+   assert(success >= 0);
+
+   dst_rect.x = 0;
+   dst_rect.y = 0;
+   dst_rect.width = state.screen_width;
+   dst_rect.height = state.screen_height;
+      
+   src_rect.x = 0;
+   src_rect.y = 0;
+   src_rect.width = state.screen_width << 16;
+   src_rect.height = state.screen_height << 16;        
+
+   state.dispman_display = vc_dispmanx_display_open(0 /* LCD */);
+   dispman_update = vc_dispmanx_update_start(0);
+         
+   state.dispman_element = vc_dispmanx_element_add (dispman_update, state.dispman_display,
+      0/*layer*/, &dst_rect, 0/*src*/,
+      &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
+      
+   nativewindow.element = state.dispman_element;
+   nativewindow.width = state.screen_width;
+   nativewindow.height = state.screen_height;
+   vc_dispmanx_update_submit_sync(dispman_update);
+
+   state.surface = eglCreateWindowSurface(state.display, config, &nativewindow, NULL);
+   assert(state.surface != EGL_NO_SURFACE);
+
+   // connect the context to the surface
+   result = eglMakeCurrent(state.display, state.surface, state.surface, state.context);
+   assert(EGL_FALSE != result);
+
+   // Set background color and clear buffers
+   glClearColor(0.15f, 0.25f, 0.35f, 1.0f);
+
+   // Enable back face culling.
+   glEnable(GL_CULL_FACE);
+
+   // Not in GLES2? TODO check glMatrixMode(GL_MODELVIEW);
 #else
     if (!glfwInit()) {
         SEND_ERLANG_ERR("Failed to init GLFW.");
