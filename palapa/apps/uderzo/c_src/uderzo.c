340b40c85d49b83b76fa511dbf38e6f73c305262
Cees de Groot <casedeg@gmail.com>
2018-05-07 20:26:25 -0400

    Move to elixir_make

--- palapa/apps/uderzo/c_src/uderzo.c
+++ palapa/apps/uderzo/c_src/uderzo.c
@@ -381,7 +381,7 @@ static void _dispatch_comment(const char *buf, unsigned short len, int *index) {
 }
 
 /* ANSI-C code produced by gperf version 3.1 */
-/* Command-line: /usr/bin/gperf -t /tmp/clixir-temp-nonode@nohost--576460752303422073.gperf  */
+/* Command-line: /usr/bin/gperf -t /tmp/clixir-temp-nonode@nohost--576460752303423487.gperf  */
 /* Computed positions: -k'1' */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
@@ -411,7 +411,7 @@ static void _dispatch_comment(const char *buf, unsigned short len, int *index) {
 #error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
 #endif
 
-#line 1 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 1 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
 struct dispatch_entry {
   char *name;
   void (*dispatch_func)(const char *buf, unsigned short len, int *index);
@@ -472,24 +472,24 @@ in_word_set (register const char *str, register size_t len)
   static struct dispatch_entry wordlist[] =
     {
       {""}, {""}, {""}, {""}, {""}, {""}, {""},
-#line 12 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 12 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"comment", _dispatch_comment},
       {""},
-#line 6 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 6 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"draw_eyes", _dispatch_draw_eyes},
       {""},
-#line 7 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 7 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"demo_render", _dispatch_demo_render},
       {""}, {""}, {""}, {""},
-#line 8 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 8 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"uderzo_end_frame", _dispatch_uderzo_end_frame},
       {""},
-#line 9 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 9 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"uderzo_start_frame", _dispatch_uderzo_start_frame},
       {""}, {""}, {""}, {""},
-#line 11 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 11 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"glfw_create_window", _dispatch_glfw_create_window},
-#line 10 "/tmp/clixir-temp-nonode@nohost--576460752303422073.gperf"
+#line 10 "/tmp/clixir-temp-nonode@nohost--576460752303423487.gperf"
       {"glfw_destroy_window", _dispatch_glfw_destroy_window}
     };
 
--- palapa/apps/uderzo/mix.exs
+++ palapa/apps/uderzo/mix.exs
@@ -1,26 +1,3 @@
-defmodule Mix.Tasks.Compile.Native do
-  def make_path do
-    if Mix.Project.umbrella? do
-      "apps/uderzo"
-    else
-      "."
-    end
-  end
-  def run(_) do
-    case :os.type do
-      {_, :linux} ->
-        ## Hack alert
-        {result, error} = System.cmd("make", ["-f", "Makefile.linux", "compile"], stderr_to_stdout: true, cd: make_path())
-        IO.binwrite(result)
-        0 = error
-      _ ->
-        IO.warn("Operating system not yet supported")
-        exit(1)
-    end
-    :ok
-  end
-end
-
 defmodule Uderzo.Mixfile do
   use Mix.Project
 
@@ -35,12 +12,8 @@ defmodule Uderzo.Mixfile do
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      compilers: Mix.compilers ++ [:native]
+      compilers: Mix.compilers ++ [:elixir_make]
     ]
-  end
-
-  def clixir do
-    ["c_src/uderzo"]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -54,9 +27,7 @@ defmodule Uderzo.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:elixir_make, "~> 0.4", runtime: false},
       {:mix_test_watch, "~> 0.3", only: [:dev, :test]}
     ]
   end
