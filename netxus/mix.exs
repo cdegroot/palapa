4ee0e84195db781d4cc56f4e9cf9b74995b2f00d
Cees de Groot <casedeg@gmail.com>
2020-01-27 19:41:58 -0500

    Move to umbrella

--- netxus/mix.exs
+++ netxus/mix.exs
@@ -3,26 +3,19 @@ defmodule Netxus.MixProject do
 
   def project do
     [
-      app: :netxus,
+      apps_path: "apps",
       version: "0.1.0",
-      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
--- netxus/test/netxus_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NetxusTest do
-  use ExUnit.Case
-  doctest Netxus
-
-  test "greets the world" do
-    assert Netxus.hello() == :world
-  end
-end
